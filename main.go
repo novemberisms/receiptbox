51f24d892e689f4feaccad590d436ff8a8191cac
Novemberisms <briansarfati@gmail.com>
2019-08-21 18:52:25 +0800

    Initial Commit

---
+++ main.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+// const address = "http://localhost:3885/"
+const address = "http://192.168.7.91:3885/"
+
+func main() {
+
+	printPrelude()
+
+	for {
+		recordEntry()
+	}
+
+}
+
+func printPrelude() {
+
+	fmt.Println("==================================================================")
+	fmt.Println("Receipt Box!")
+	fmt.Println("type in 'oops' to reset the current entry if you've made a mistake")
+	fmt.Println("==================================================================")
+
+}
+
+func recordEntry() {
+	defer fmt.Println("------------------------------------------------------------------")
+
+	inputReader := bufio.NewReader(os.Stdin)
+
+	fmt.Print("Date (mm-dd)     > ")
+	date, isOops := readString(inputReader)
+	if isOops {
+		return
+	}
+
+	fmt.Print("Restaurant       > ")
+	restaurant, isOops := readString(inputReader)
+	if isOops {
+		return
+	}
+
+	fmt.Print("Amount           > ")
+	amount, isOops := readString(inputReader)
+	if isOops {
+		return
+	}
+
+	fmt.Print("Submit?          > ")
+	_, isOops = readString(inputReader)
+	if isOops {
+		return
+	}
+
+	fmt.Print("...")
+
+	message := map[string]string{
+		"date":       date,
+		"restaurant": restaurant,
+		"amount":     amount,
+	}
+
+	messageAsBytes, err := json.Marshal(message)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	data := bytes.NewBuffer(messageAsBytes)
+
+	response, err := http.Post(address, "application/json", data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(body))
+}
+
+func readString(reader *bufio.Reader) (value string, isOops bool) {
+	value, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+	value = strings.TrimRight(value, "\r\n")
+	isOops = value == "oops"
+	return
+}
